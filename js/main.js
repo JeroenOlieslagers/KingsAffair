462154b9cee8138db53856540eef88e1382b8038
Charles J.Y. Yoon <jyy24@cam.ac.uk>
2018-02-25 21:10:25 +0000

    Fixed parallax

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-$(function() {
-    var $el = $('.background');
-    $(window).on('scroll', function () {
-        var scroll = $(document).scrollTop();
-        $el.css({
-            'background-position':'50% '+(0.4*scroll)+'px'
-        });
-    });
-});
+
