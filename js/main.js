ec3b39bcf1664cf7ecfdd2105b3beba5ba6f5336
Charles J.Y. Yoon <1242029+jaeyeun97@users.noreply.github.com>
2018-02-27 18:41:06 +0000

    Semi Done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+$.getJSON('https://vimeo.com/api/oembed.json?url=https%3A%2F%2Fvimeo.com%2F225490082%2Fc80dd964a2', function(data){
+	$('.youtube').append(data.html);
+	var video = $('.youtube iframe').data('aspectRatio', (data.height / data.width))
+								.removeAttr('width')
+								.removeAttr('height');
+	var width = $('.youtube').width();
+	var height = width  * video.data('aspectRatio');
+	video.width(width).height(height);
+});
 
+$(window).resize(function(){
+	var video = $('.youtube iframe');
+	var width = $('.youtube').width();
+	var height = width  * video.data('aspectRatio');
+	video.width(width).height(height);
+});
+
+$('.sponsor .more').on('click', function(){
+	$(this).parent().siblings('.hidden').show();
+	$(this).hide();
+});
+
+$('.sponsor .less').on('click', function(){
+	$(this).parents('.sponsor').find('.hidden').hide();
+	$(this).parents('.sponsor').find('.more').show();
+});
